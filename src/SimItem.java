a9a6c6c9e57701d1b511dcb26e7b2f186b77aa0b
Jack.mooney@my.jcu.edu.au <mooney12>
2019-10-23 17:02:33 +1000

    Specified base values for X & Y coordinates. Getters and Setters for X & Y. Added draw() method (no objects as of this commit).

--- src/SimItem.java
+++ src/SimItem.java
@@ -1,2 +1,34 @@
-public abstract class SimItem {
+public abstract class SimItem implements Drawable {
+    private double x = 0.0;
+    private double y = 0.0;
+
+    public SimItem(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public double getX() {
+        return x;
+    }
+
+    @Override
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    @Override
+    public void setY(double y) {
+        this.y = y;
+    }
 }
