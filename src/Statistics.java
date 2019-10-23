e07a03dc2ba7c463ad1416f08c4cd4622788f9cb
Jack.mooney@my.jcu.edu.au <mooney12>
2019-10-23 17:57:27 +1000

    Create city instance and get average speed (not sure what to do inside city class yet).

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,2 +1,12 @@
+
 public class Statistics {
+    private final City city;
+
+    public Statistics(City city) {
+        this.city = city;
+    }
+
+    public double getAverageSpeed() {
+        return 0.0;
+    }
 }
