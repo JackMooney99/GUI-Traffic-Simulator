8a0d21776ccef97e950c97858ced982e82e63833
Jack.mooney@my.jcu.edu.au <mooney12>
2019-10-23 17:52:07 +1000

    Initialized the parameters in which inherits from vehicle. Added length()

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,20 @@
-public class Car {
+public class Car extends Vehicle {
+    /**
+     * Constructor
+     *
+     * @param x
+     * @param y
+     * @param road    a road segment
+     * @param roadPos a position on the road
+     * @param dir
+     * @param speed   a current speed of the car
+     */
+    public Car(double x, double y, Road road, int roadPos, int dir, int speed) {
+        super(x, y, road, roadPos, dir, speed);
+    }
+
+    @Override
+    public double length() {
+        return LENGTH;
+    }
 }
--- src/Motorbike.java
+++ src/Motorbike.java
@@ -1,2 +1,20 @@
-public class Motorbike {
+public class Motorbike extends Vehicle {
+    /**
+     * Constructor
+     *
+     * @param x
+     * @param y
+     * @param road    a road segment
+     * @param roadPos a position on the road
+     * @param dir
+     * @param speed   a current speed of the car
+     */
+    public Motorbike(double x, double y, Road road, int roadPos, int dir, int speed) {
+        super(x, y, road, roadPos, dir, speed);
+    }
+
+    @Override
+    public double length() {
+        return 0.5 * LENGTH;
+    }
 }
