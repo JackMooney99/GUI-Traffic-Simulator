99f30a9a02024c07d9d0c628ff89e60212eaeecf
Jack.mooney@my.jcu.edu.au <mooney12>
2019-10-23 17:21:44 +1000

    Importation of Variables and Methods

--- src/Road.java
+++ src/Road.java
@@ -1,2 +1,55 @@
-public class Road {
+/*
+ * Represents a road segment
+ */
+public class Road extends SimItem {
+    /*
+     * the length
+     */
+    private int length;
+    /*
+     * the next connected road segment
+     */
+    private Road next;
+
+    /**
+     * Constructor
+     * @param length the length
+     */
+    public Road(double x, double y, int length) {
+        super(x, y);
+        setLength(length);
+        setNext(null);
+    }
+
+    /*
+     * @return the length
+     */
+    public int getLength() {
+        return length;
+    }
+
+    /**
+     *
+     * @param length the length
+     */
+    public void setLength(int length) {
+        if (length <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.length = length;
+    }
+
+    /**
+     * @return the next connected road segment
+     */
+    public Road getNext() {
+        return next;
+    }
+
+    /**
+     * @param next the next connected road segment
+     */
+    public void setNext(Road next) {
+        this.next = next;
+    }
 }
--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,2 +1,78 @@
-public class TrafficLight {
+import java.util.Random;
+
+/*
+ * Represents a Traffic Light
+ */
+public class TrafficLight extends SimItem {
+    private String colour;
+    private double rate;
+
+    /**
+     * Constructor
+     * @param colour the colour
+     * @param rate the rate
+     */
+    public TrafficLight(double x, double y, String colour, double rate) {
+        super(x, y);
+        setColour(colour);
+        setRate(rate);
+    }
+
+    /**
+     * @return the colour
+     */
+    public String getColour() {
+        return colour;
+    }
+
+    /**
+     * @param colour the colour
+     */
+    public void setColour(String colour) {
+        if (colour.equals("green") || colour.equals("red")) {
+            this.colour = colour;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * If colour is "red" then set colour to "green"
+     * If colour is "green" then set colour to "red"
+     */
+    public void toggleColour() {
+        if (getColour().equals("red")) {
+            setColour("green");
+        } else {
+            setColour("red");
+        }
+    }
+
+    /**
+     * @return the rate
+     */
+    public double getRate() {
+        return rate;
+    }
+
+    /**
+     * @param rate the rate
+     */
+    public void setRate(double rate) {
+        if (rate >= 0.0 && rate <= 1.0) {
+            this.rate = rate;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /*
+     * Traffic Light operation
+     */
+    public void operate() {
+        double randomRate = new Random().nextDouble();
+        if (randomRate < getRate()) {
+            toggleColour();
+        }
+    }
 }
