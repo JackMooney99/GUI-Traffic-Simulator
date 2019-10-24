a74261350aa7d0d3b06168a3dc851a69003b437c
Jack.mooney@my.jcu.edu.au <mooney12>
2019-10-24 16:26:31 +1000

    +Instance of CityView

--- src/CityView.java
+++ src/CityView.java
@@ -1,2 +1,13 @@
-public class CityView {
+import javax.swing.*;
+
+public class CityView extends JPanel {
+    private final City city;
+
+    public CityView(City city) {
+        this.city = city;
+    }
+
+    public City getCity() {
+        return city;
+    }
 }
