459f327096719a3a9a9f1582e89ea14165d12297
Jack.mooney@my.jcu.edu.au <mooney12>
2019-10-24 15:28:27 +1000

    NOTE: Still needs more work. New array for objects (vehicle, TrafficLight, Road) add objects to the city and Enable objects operational method to step()

--- src/City.java
+++ src/City.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+
 public class City {
+    private final ArrayList<Vehicle> vehicles = new ArrayList<>();
+    private final ArrayList<TrafficLight> trafficLights = new ArrayList<>();
+    private final ArrayList<Road> roads = new ArrayList<>();
+
+    public void addVehicle(Vehicle v) {
+        vehicles.add(v);
+    }
+
+    public void addTrafficLight(TrafficLight tl) {
+        trafficLights.add(tl);
+    }
+
+    public void addRoad(Road r) {
+        roads.add(r);
+    }
+
+    public ArrayList<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public ArrayList<TrafficLight> getTrafficLights() {
+        return trafficLights;
+    }
+
+    public ArrayList<Road> getRoads() {
+        return roads;
+    }
+
+    public void step() {
+        for (Vehicle v: vehicles) {
+            v.move();
+        }
+
+        for (TrafficLight tl: trafficLights) {
+            tl.operate();
+        }
+    }
 }
