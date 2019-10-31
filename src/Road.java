fe463d13b913e4886e1fe6a0ed01f68e94f26232
Jack.mooney@my.jcu.edu.au <mooney12>
2019-10-31 23:29:05 +1000

    Road Logic: Handles the logic for the direction of the vehicle and turning onto the next road

--- src/Road.java
+++ src/Road.java
@@ -1,63 +1,71 @@
-/*
+import java.util.ArrayList;
+
+/**
  * Represents a road segment
  */
-public class Road /*extends SimItem*/ {
-    public static final int LENGTH = 100;
-    /*
-     * the length
-     */
-    private int length;
-    /*
-     * the next connected road segment
-     */
-    private Road next;
+public class Road extends SimItem {
+    static final int LENGTH = 10;
 
-    /*
+    private final CityCell cityCell;
+
+    private final Direction direction;
+
+    private final ArrayList<Vehicle> vehicles;
+
+    /**
      * Default constructor
      */
-    public Road() {
-        setLength(LENGTH);
-        setNext(null);
+    public Road(CityCell cityCell, Direction direction) {
+        this.cityCell = cityCell;
+        this.direction = direction;
+        this.vehicles = new ArrayList<>();
     }
 
-    /**
-     * Constructor
-     * @param length the length
-     */
-    public Road(double x, double y, int length) {
-        //super(x, y);
-        setLength(length);
-        setNext(null);
+    ArrayList<Vehicle> getVehicles() {
+        return vehicles;
     }
 
-    /**
-     * @return the length
-     */
-    public int getLength() {
-        return length;
+    Direction getDirection() {
+        return direction;
     }
 
-    /**
-     * @param length the length
-     */
-    public void setLength(int length) {
-        if (length <= 0) {
-            throw new IllegalArgumentException();
+    public CityCell getCityCell() {
+        return cityCell;
+    }
+
+    void placeVehicle(Vehicle vehicle, int section) {
+        vehicles.add(vehicle);
+        vehicle.setRoad(this);
+        vehicle.setPosition(section);
+    }
+
+    void removeVehicle(Vehicle vehicle) {
+        vehicles.remove(vehicle);
+        vehicle.setRoad(null);
+    }
+
+    Road[] getAdjacentRoads() {
+        Road[] roads = new Road[cityCell.getAdjacentRoads().length - 1];
+        int num = 0;
+        for (Road r: cityCell.getAdjacentRoads()) {
+            if (r != this) {
+                roads[num++] = r;
+            }
         }
-        this.length = length;
+        return roads;
     }
 
-    /**
-     * @return the next connected road segment
-     */
-    public Road getNext() {
-        return next;
-    }
+    Road nextRoad() {
+        if (direction == Direction.RIGHT && cityCell.getX() < City.CITY_WIDTH - 1) {
+            return cityCell.getCity().getCityCells()[cityCell.getY()][cityCell.getX() + 1].getRoad(Direction.LEFT);
+        } else if (direction == Direction.LEFT && cityCell.getX() > 0) {
+            return cityCell.getCity().getCityCells()[cityCell.getY()][cityCell.getX() - 1].getRoad(Direction.RIGHT);
+        } if (direction == Direction.DOWN && cityCell.getY() < City.CITY_HEIGHT - 1) {
+            return cityCell.getCity().getCityCells()[cityCell.getY() + 1][cityCell.getX()].getRoad(Direction.UP);
+        } else if (direction == Direction.UP && cityCell.getY() > 0) {
+            return cityCell.getCity().getCityCells()[cityCell.getY() - 1][cityCell.getX()].getRoad(Direction.DOWN);
+        }
 
-    /**
-     * @param next the next connected road segment
-     */
-    public void setNext(Road next) {
-        this.next = next;
+        return null;
     }
 }
